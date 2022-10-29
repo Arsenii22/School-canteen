654c01b670f9d521da8a7961af08128023ee1589
Arsenii <MiniDinik@gmail.com>
2022-10-29 21:21:48 +0200

    sus2

--- bot.py
+++ bot.py
@@ -4,10 +4,11 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 import config
 import json
 
-logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
@@ -32,7 +33,13 @@ async def start(msg: types.Message):
 
 @dp.message_handler(state=Form.school)
 async def get_school(msg: types.Message, state: FSMContext):
-    await msg.reply(f"Ваша школа: {msg.text}", reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton("Да", callback_data="yes")).add(InlineKeyboardButton("Нет", callback_data="no")))
+    schools = ["СОШ №6", "МАОУ гимназия №1", "МАОУ гимназия №22", "МАОУ гимназия №32", "МАОУ гимназия №40", "МАОУ морской лицей", "МАОУ лицей №23", "МАОУ лицей №17", "МАОУ лицей №18", "МАОУ лицей №35", "МАОУ ШИЛИ", "Школа-детский сад №72", "СОШ №10", "СОШ №11", "СОШ №12", "СОШ №56", "СОШ №8", "CОШ №9", "СОШ №13", "СОШ №14", "CОШ №15", "СОШ №16", "СОШ №19", "СОШ №2", "СОШ №21", "СОШ №24", "СОШ №25", "СОШ №3", "СОШ №27", "СОШ №28", "СОШ №36", "СОШ №30", "СОШ №31", "СОШ №32", "СОШ №38", "СОШ №39", "СОШ №44", "СОШ №45", "СОШ №46", "СОШ №47", "СОШ №48", "СОШ №49", "СОШ №50", "СОШ №41", "СОШ №5", "СОШ №53", "СОШ №7", "СОШ №43", "Вечерняя школа №17"]
+    best_school = process.extractOne(msg.text, schools)
+
+    if best_school[1] != 0:
+        await msg.reply(f"Ваша школа: {best_school[0]}?", reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton("Да", callback_data="yes")).add(InlineKeyboardButton("Нет", callback_data="no")))
+    else:
+        await msg.reply(f"Мы не смогли найти вашу школу, попробуйте ещё раз :(")
 
 @dp.callback_query_handler(text="yes", state=Form.all_states)
 @dp.callback_query_handler(text="no", state=Form.all_states)
@@ -41,7 +48,7 @@ async def answers(query: types.CallbackQuery, state: FSMContext):
 
     if query.data == "yes" and query.message.text.startswith("Ваша школа:"):
         async with state.proxy() as data:
-            data["school"] = query.message.text.replace("Ваша школа: ", "")
+            data["school"] = query.message.text.replace("Ваша школа: ", "").replace("?", "")
         await Form.next()
 
         await query.message.edit_reply_markup()
@@ -57,13 +64,14 @@ async def answers(query: types.CallbackQuery, state: FSMContext):
         await Form.next()
 
         await query.message.edit_reply_markup()
-        await query.message.reply("Что вам больше всего понравилось?", reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton("Свежая еда", callback_data="opinion_fresh")).add(InlineKeyboardButton("вкусно", callback_data="opinion_vkusno")).add(InlineKeyboardButton("аппетитно", callback_data="opinion_appetitno")).add(InlineKeyboardButton("сытно вкусно", callback_data="opinion_very_very_vkusno")))
+        await query.message.reply("Что вам больше всего понравилось?", reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton("Свежая еда", callback_data="opinion_fresh")).add(InlineKeyboardButton("вкусная еда", callback_data="opinion_vkusno")).add(InlineKeyboardButton("аппетитно", callback_data="opinion_appetitno")).add(InlineKeyboardButton("сытно вкусно", callback_data="opinion_very_very_vkusno")))
 
     elif query.data == "no" and query.message.text == "Нравится ли вам еда в школьной столовой?":
         async with state.proxy() as data:
             data["like"] = False
         await Form.next()
 
+        await query.message.edit_reply_markup()
         await query.message.reply("Что вам больше всего не понравилось?", reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton("НЕ Свежая еда", callback_data="opinion_not_fresh")).add(InlineKeyboardButton("НЕ Очень вкусно", callback_data="opinion_not_vkusno")).add(InlineKeyboardButton("НЕ Очень очень вкусно", callback_data="opinion_not_very_vkusno")).add(InlineKeyboardButton("НЕ Очень очень очень вкусно", callback_data="opinion_not_very_very_vkusno")))
 
 
@@ -73,6 +81,7 @@ async def answers_continue(query: types.CallbackQuery, state: FSMContext):
         data["opinion"] = query.data
     await Form.next()
 
+    await query.message.edit_reply_markup()
     await query.message.reply("Оцените качество еды по 10 балльной шкале", reply_markup=ReplyKeyboardMarkup().add(KeyboardButton("1️⃣")).add(KeyboardButton("2️⃣")).add(KeyboardButton("3️⃣")).add(KeyboardButton("4️⃣")).add(KeyboardButton("5️⃣")).add(KeyboardButton("6️⃣")).add(KeyboardButton("7️⃣")).add(KeyboardButton("8️⃣")).add(KeyboardButton("9️⃣")).add(KeyboardButton("🔟")))
 
 @dp.message_handler(lambda t: t.text in "1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣8️⃣9️⃣🔟", state=Form.all_states)
@@ -86,4 +95,4 @@ async def get_rate(msg: types.Message, state: FSMContext):
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
